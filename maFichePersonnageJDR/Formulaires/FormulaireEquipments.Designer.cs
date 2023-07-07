49032ac9d0e7573fa52a7882e493065f8d271a9a
Gabriel DELAGE <gabriel.delage@lyc-bonaparte.fr>
2023-07-07 23:07:21 +0200

    init branche 03-mise-a-jour-form-equipement-magie; ajout form equipment;

---
+++ maFichePersonnageJDR/Formulaires/FormulaireEquipments.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace maFichePersonnageJDR.Formulaires
+{
+    partial class FormulaireEquipments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCntlArmes = new System.Windows.Forms.TabControl();
+            this.tbPgeEpee = new System.Windows.Forms.TabPage();
+            this.tbPgeLance = new System.Windows.Forms.TabPage();
+            this.tbPgePoignard = new System.Windows.Forms.TabPage();
+            this.tbPgeHache = new System.Windows.Forms.TabPage();
+            this.tbPgeMasse = new System.Windows.Forms.TabPage();
+            this.tbPgeArmTrt = new System.Windows.Forms.TabPage();
+            this.tbPgeChaines = new System.Windows.Forms.TabPage();
+            this.tbPgeBaton = new System.Windows.Forms.TabPage();
+            this.tbCntlArmures = new System.Windows.Forms.TabControl();
+            this.tbPgeCasque = new System.Windows.Forms.TabPage();
+            this.tbPgeBustes = new System.Windows.Forms.TabPage();
+            this.tbPgeGants = new System.Windows.Forms.TabPage();
+            this.tbPgeGenouilleres = new System.Windows.Forms.TabPage();
+            this.tbPgeChaussure = new System.Windows.Forms.TabPage();
+            this.tbPgeBouclier = new System.Windows.Forms.TabPage();
+            this.tbCntlObjets = new System.Windows.Forms.TabControl();
+            this.tbPgeObjNormaux = new System.Windows.Forms.TabPage();
+            this.tbPgeObjRares = new System.Windows.Forms.TabPage();
+            this.tbCntlArmes.SuspendLayout();
+            this.tbCntlArmures.SuspendLayout();
+            this.tbCntlObjets.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbCntlArmes
+            // 
+            this.tbCntlArmes.Controls.Add(this.tbPgeEpee);
+            this.tbCntlArmes.Controls.Add(this.tbPgeLance);
+            this.tbCntlArmes.Controls.Add(this.tbPgePoignard);
+            this.tbCntlArmes.Controls.Add(this.tbPgeHache);
+            this.tbCntlArmes.Controls.Add(this.tbPgeMasse);
+            this.tbCntlArmes.Controls.Add(this.tbPgeArmTrt);
+            this.tbCntlArmes.Controls.Add(this.tbPgeChaines);
+            this.tbCntlArmes.Controls.Add(this.tbPgeBaton);
+            this.tbCntlArmes.Location = new System.Drawing.Point(12, 12);
+            this.tbCntlArmes.Name = "tbCntlArmes";
+            this.tbCntlArmes.SelectedIndex = 0;
+            this.tbCntlArmes.Size = new System.Drawing.Size(327, 168);
+            this.tbCntlArmes.TabIndex = 0;
+            // 
+            // tbPgeEpee
+            // 
+            this.tbPgeEpee.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeEpee.Name = "tbPgeEpee";
+            this.tbPgeEpee.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgeEpee.Size = new System.Drawing.Size(319, 142);
+            this.tbPgeEpee.TabIndex = 0;
+            this.tbPgeEpee.Text = "Épées";
+            this.tbPgeEpee.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeLance
+            // 
+            this.tbPgeLance.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeLance.Name = "tbPgeLance";
+            this.tbPgeLance.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgeLance.Size = new System.Drawing.Size(192, 74);
+            this.tbPgeLance.TabIndex = 1;
+            this.tbPgeLance.Text = "Lances";
+            this.tbPgeLance.UseVisualStyleBackColor = true;
+            // 
+            // tbPgePoignard
+            // 
+            this.tbPgePoignard.Location = new System.Drawing.Point(4, 22);
+            this.tbPgePoignard.Name = "tbPgePoignard";
+            this.tbPgePoignard.Size = new System.Drawing.Size(192, 74);
+            this.tbPgePoignard.TabIndex = 2;
+            this.tbPgePoignard.Text = "Poignards";
+            this.tbPgePoignard.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeHache
+            // 
+            this.tbPgeHache.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeHache.Name = "tbPgeHache";
+            this.tbPgeHache.Size = new System.Drawing.Size(192, 74);
+            this.tbPgeHache.TabIndex = 3;
+            this.tbPgeHache.Text = "Haches";
+            this.tbPgeHache.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeMasse
+            // 
+            this.tbPgeMasse.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeMasse.Name = "tbPgeMasse";
+            this.tbPgeMasse.Size = new System.Drawing.Size(192, 74);
+            this.tbPgeMasse.TabIndex = 4;
+            this.tbPgeMasse.Text = "Masses";
+            this.tbPgeMasse.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeArmTrt
+            // 
+            this.tbPgeArmTrt.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeArmTrt.Name = "tbPgeArmTrt";
+            this.tbPgeArmTrt.Size = new System.Drawing.Size(192, 74);
+            this.tbPgeArmTrt.TabIndex = 5;
+            this.tbPgeArmTrt.Text = "Armes de trait";
+            this.tbPgeArmTrt.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeChaines
+            // 
+            this.tbPgeChaines.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeChaines.Name = "tbPgeChaines";
+            this.tbPgeChaines.Size = new System.Drawing.Size(192, 74);
+            this.tbPgeChaines.TabIndex = 6;
+            this.tbPgeChaines.Text = "Chaînes";
+            this.tbPgeChaines.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeBaton
+            // 
+            this.tbPgeBaton.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeBaton.Name = "tbPgeBaton";
+            this.tbPgeBaton.Size = new System.Drawing.Size(192, 74);
+            this.tbPgeBaton.TabIndex = 7;
+            this.tbPgeBaton.Text = "Bâtons";
+            this.tbPgeBaton.UseVisualStyleBackColor = true;
+            // 
+            // tbCntlArmures
+            // 
+            this.tbCntlArmures.Controls.Add(this.tbPgeCasque);
+            this.tbCntlArmures.Controls.Add(this.tbPgeBustes);
+            this.tbCntlArmures.Controls.Add(this.tbPgeGants);
+            this.tbCntlArmures.Controls.Add(this.tbPgeGenouilleres);
+            this.tbCntlArmures.Controls.Add(this.tbPgeChaussure);
+            this.tbCntlArmures.Controls.Add(this.tbPgeBouclier);
+            this.tbCntlArmures.Location = new System.Drawing.Point(367, 12);
+            this.tbCntlArmures.Name = "tbCntlArmures";
+            this.tbCntlArmures.SelectedIndex = 0;
+            this.tbCntlArmures.Size = new System.Drawing.Size(280, 168);
+            this.tbCntlArmures.TabIndex = 1;
+            // 
+            // tbPgeCasque
+            // 
+            this.tbPgeCasque.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeCasque.Name = "tbPgeCasque";
+            this.tbPgeCasque.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgeCasque.Size = new System.Drawing.Size(272, 142);
+            this.tbPgeCasque.TabIndex = 0;
+            this.tbPgeCasque.Text = "Casques";
+            this.tbPgeCasque.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeBustes
+            // 
+            this.tbPgeBustes.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeBustes.Name = "tbPgeBustes";
+            this.tbPgeBustes.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgeBustes.Size = new System.Drawing.Size(263, 142);
+            this.tbPgeBustes.TabIndex = 1;
+            this.tbPgeBustes.Text = "Bustes";
+            this.tbPgeBustes.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeGants
+            // 
+            this.tbPgeGants.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeGants.Name = "tbPgeGants";
+            this.tbPgeGants.Size = new System.Drawing.Size(263, 142);
+            this.tbPgeGants.TabIndex = 2;
+            this.tbPgeGants.Text = "Gants";
+            this.tbPgeGants.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeGenouilleres
+            // 
+            this.tbPgeGenouilleres.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeGenouilleres.Name = "tbPgeGenouilleres";
+            this.tbPgeGenouilleres.Size = new System.Drawing.Size(263, 142);
+            this.tbPgeGenouilleres.TabIndex = 3;
+            this.tbPgeGenouilleres.Text = "Genouillères";
+            this.tbPgeGenouilleres.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeChaussure
+            // 
+            this.tbPgeChaussure.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeChaussure.Name = "tbPgeChaussure";
+            this.tbPgeChaussure.Size = new System.Drawing.Size(263, 142);
+            this.tbPgeChaussure.TabIndex = 4;
+            this.tbPgeChaussure.Text = "Chaussures";
+            this.tbPgeChaussure.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeBouclier
+            // 
+            this.tbPgeBouclier.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeBouclier.Name = "tbPgeBouclier";
+            this.tbPgeBouclier.Size = new System.Drawing.Size(263, 142);
+            this.tbPgeBouclier.TabIndex = 5;
+            this.tbPgeBouclier.Text = "Boucliers";
+            this.tbPgeBouclier.UseVisualStyleBackColor = true;
+            // 
+            // tbCntlObjets
+            // 
+            this.tbCntlObjets.Controls.Add(this.tbPgeObjNormaux);
+            this.tbCntlObjets.Controls.Add(this.tbPgeObjRares);
+            this.tbCntlObjets.Location = new System.Drawing.Point(679, 12);
+            this.tbCntlObjets.Name = "tbCntlObjets";
+            this.tbCntlObjets.SelectedIndex = 0;
+            this.tbCntlObjets.Size = new System.Drawing.Size(284, 164);
+            this.tbCntlObjets.TabIndex = 2;
+            // 
+            // tbPgeObjNormaux
+            // 
+            this.tbPgeObjNormaux.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeObjNormaux.Name = "tbPgeObjNormaux";
+            this.tbPgeObjNormaux.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgeObjNormaux.Size = new System.Drawing.Size(276, 138);
+            this.tbPgeObjNormaux.TabIndex = 0;
+            this.tbPgeObjNormaux.Text = "Objets normaux";
+            this.tbPgeObjNormaux.UseVisualStyleBackColor = true;
+            // 
+            // tbPgeObjRares
+            // 
+            this.tbPgeObjRares.Location = new System.Drawing.Point(4, 22);
+            this.tbPgeObjRares.Name = "tbPgeObjRares";
+            this.tbPgeObjRares.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgeObjRares.Size = new System.Drawing.Size(276, 138);
+            this.tbPgeObjRares.TabIndex = 1;
+            this.tbPgeObjRares.Text = "Objets rares";
+            this.tbPgeObjRares.UseVisualStyleBackColor = true;
+            // 
+            // FormulaireEquipments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 204);
+            this.Controls.Add(this.tbCntlObjets);
+            this.Controls.Add(this.tbCntlArmures);
+            this.Controls.Add(this.tbCntlArmes);
+            this.Name = "FormulaireEquipments";
+            this.Text = " Équipements";
+            this.tbCntlArmes.ResumeLayout(false);
+            this.tbCntlArmures.ResumeLayout(false);
+            this.tbCntlObjets.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tbCntlArmes;
+        private System.Windows.Forms.TabPage tbPgeEpee;
+        private System.Windows.Forms.TabPage tbPgeLance;
+        private System.Windows.Forms.TabPage tbPgePoignard;
+        private System.Windows.Forms.TabPage tbPgeHache;
+        private System.Windows.Forms.TabPage tbPgeMasse;
+        private System.Windows.Forms.TabPage tbPgeArmTrt;
+        private System.Windows.Forms.TabPage tbPgeChaines;
+        private System.Windows.Forms.TabPage tbPgeBaton;
+        private System.Windows.Forms.TabControl tbCntlArmures;
+        private System.Windows.Forms.TabPage tbPgeCasque;
+        private System.Windows.Forms.TabPage tbPgeBustes;
+        private System.Windows.Forms.TabPage tbPgeGants;
+        private System.Windows.Forms.TabPage tbPgeGenouilleres;
+        private System.Windows.Forms.TabPage tbPgeChaussure;
+        private System.Windows.Forms.TabPage tbPgeBouclier;
+        private System.Windows.Forms.TabControl tbCntlObjets;
+        private System.Windows.Forms.TabPage tbPgeObjNormaux;
+        private System.Windows.Forms.TabPage tbPgeObjRares;
+    }
+}
