9036f787d2fc7635e27d24fcdfd567f5de4d35a2
delageGabriel <71062337+delageGabriel@users.noreply.github.com>
2023-09-01 15:52:12 +0200

    Création classe MagieModel;

--- maFichePersonnageJDR/Model/ArmesModel.cs
+++ maFichePersonnageJDR/Model/ArmesModel.cs
@@ -1,11 +1,7 @@
-﻿using System;
+﻿using maFichePersonnageJDR.Classe;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SQLite;
-using System.Windows.Forms;
-using maFichePersonnageJDR.Classe;
 
 namespace maFichePersonnageJDR.Model
 {
